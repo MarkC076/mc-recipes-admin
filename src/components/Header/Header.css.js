b404bb345beb9899ae9a7a734f1503c635ecf829
MarkC076 <chilton.mark@gmail.com>
2019-02-28 20:12:25 +0000

    Added first test component and emotion css

---
+++ src/components/Header/Header.css.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+const HeaderStyles = styled.div/*css*/`
+    height: 100px;
+    width:100%;
+    background-color: #ccc;
+`
+
+export default HeaderStyles;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import StyledDiv from "./Header.css";
+
+const header = (props) => {
+    return (<StyledDiv></StyledDiv>);
+}
+
+export default header;
